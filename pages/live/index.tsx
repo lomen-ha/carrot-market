6e7605ad5b6c24d433627718bcc286de600ace98
ha <the9616@naver.com>
2022-07-20 21:48:49 +0900

    CSS init done

--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -1,33 +1,40 @@
 import { NextPage } from 'next';
+import Link from 'next/link';
+import FloatingButton from '../../components/floating-button';
+import Layout from '../../components/layout';
 
 const Live: NextPage = () => {
   return (
-    <div className="space-y-4 divide-y-2 py-10">
-      {new Array(6).fill(1).map((_, i) => (
-        <div className="px-4 pt-4" key={i}>
-          <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
-          <h3 className="mt-2 text-lg text-gray-700">
-            Let&apos;s try potatoes
-          </h3>
-        </div>
-      ))}
-      <button className="fixed bottom-24 right-5 cursor-pointer rounded-full border-transparent bg-orange-400 p-4 text-white shadow-xl transition-colors hover:bg-orange-500">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-          strokeWidth={2}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
-          />
-        </svg>
-      </button>
-    </div>
+    <Layout hasTabBar title="라이브">
+      <div className="space-y-4 divide-y-[1px]">
+        {new Array(6).fill(1).map((_, i) => (
+          <Link key={i} href={`/live/${i}`}>
+            <a className="block px-4  pt-4">
+              <div className="aspect-video w-full rounded-md bg-slate-300 shadow-sm" />
+              <h1 className="mt-2 text-2xl font-bold text-gray-900">
+                Galaxy S50
+              </h1>
+            </a>
+          </Link>
+        ))}
+        <FloatingButton href="/live/create">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            strokeWidth={2}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
   );
 };
 
